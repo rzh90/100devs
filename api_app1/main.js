b9e5e2f5f594b87e558614ca98d9c622ee416e4a
Ruina Z <rzhao9002@gmail.com>
2022-04-09 08:58:51 -0400

    Added images of comics

--- api_app1/main.js
+++ api_app1/main.js
@@ -1,7 +1,11 @@
-fetch("https://gateway.marvel.com/v1/public/series/20607?ts=1&apikey=da494252a9e2137eeefe8855ddaf95e1&hash=8044c4979335104169fecfdccaca9811")
+fetch("https://gateway.marvel.com/v1/public/series/20607/comics?ts=1&apikey=da494252a9e2137eeefe8855ddaf95e1&hash=8044c4979335104169fecfdccaca9811")
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+        console.log(data.data.results)
+        
+        for (let i = 0; i < data.data.results.length; i++) {
+            document.querySelector(".comics").innerHTML += `<img src="${data.data.results[i].thumbnail.path}.${data.data.results[i].thumbnail.extension}">`
+        }
     })
     .catch(err => {
         console.log(`error ${err}`)
