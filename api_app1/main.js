de87299f230c77e4748d500b11bb87a8ddcff1b5
ruinaz90 <rzhao9002@gmail.com>
2022-04-09 15:57:52 -0400

    updated index.html and main.js

--- api_app1/main.js
+++ api_app1/main.js
@@ -1,10 +1,16 @@
 fetch("https://gateway.marvel.com/v1/public/series/20607/comics?noVariants=true&ts=1&apikey=da494252a9e2137eeefe8855ddaf95e1&hash=8044c4979335104169fecfdccaca9811")
     .then(res => res.json())
-    .then(data => {
-        console.log(data.data.results)
+    .then(apiData => {
+        console.log(apiData.data.results)
         
-        for (let i = data.data.results.length - 1; i >= 0; i--) {
-            document.querySelector(".comics").innerHTML += `<img src="${data.data.results[i].thumbnail.path}.${data.data.results[i].thumbnail.extension}">`
+        const comicInfo = apiData.data.results
+
+        for (let i = comicInfo.length - 1; i >= 0; i--) {
+            document.querySelector(".comics").innerHTML += 
+            `<section class="comic_info">
+                <a href="${comicInfo[i].urls[0].url}"><img src="${comicInfo[i].thumbnail.path}.${comicInfo[i].thumbnail.extension}"></a>
+                <h2>${comicInfo[i].title}</h2>
+            </section>`
         }
     })
     .catch(err => {
