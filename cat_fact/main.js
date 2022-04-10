a71060199858639a032b77ed4ce05d0e599a40cf
Ruina Z <rzhao9002@gmail.com>
2022-04-10 08:34:37 -0400

    added cat facts API

---
+++ cat_fact/main.js
@@ -0,0 +1,25 @@
+document.querySelector(".fact_button").addEventListener("click", getCatFact)
+
+function getCatFact() {
+    //Get cat image
+    fetch("https://api.thecatapi.com/v1/images/search")
+        .then(res => res.json())
+        .then(apiData => {
+            console.log(apiData[0].url)
+            document.querySelector("img").src = apiData[0].url
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+
+    //Get cat fact
+    fetch("https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1")
+        .then(res => res.json())
+        .then(apiData2 => {
+            console.log(apiData2.text)
+            document.querySelector("h2").innerText = apiData2.text
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
