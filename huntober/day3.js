15b88d2c983e165b707a72f933ac65dee61f64d3
ruinaz90 <rzhao9002@gmail.com>
2022-10-04 04:03:14 -0400

    Coding Challenge Day 3

---
+++ huntober/day3.js
@@ -0,0 +1,10 @@
+// replace any instances of any of the key characters in a given string with an empty space (' ')
+
+function removeKey(message) {
+    const key = /[Space]/g
+    //const key = /[AKEY_!]/g
+    return message.replace(key, ' ')
+}
+
+const string = 'AyouEcould!thinkKthisAisYhard_toYreadKbeforeYreplacingEthe_keyYcharacters'
+console.log(removeKey(string))
