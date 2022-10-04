f66a207150c23c65f1645f5c1ce8936df417ee1d
ruinaz90 <52875611+ruinaz90@users.noreply.github.com>
2022-10-04 18:41:49 -0400

    Create Week 1.js

---
+++ huntober/Week 1.js
@@ -0,0 +1,43 @@
+function fixChars(phrase) {
+    const replacement = {
+        "O": "0",
+        "I": "1",
+        "Z": "2",
+        "E": "3",
+        "h": "4",
+        "S": "5",
+        "G": "6",
+        "L": "7",
+        "B": "8",
+        "q": "9",
+        "0": "O",
+        "1": "I",
+        "2": "Z",
+        "3": "E",
+        "4": "h",
+        "5": "S",
+        "6": "G",
+        "7": "L",
+        "8": "B",
+        "9": "q"
+    }
+
+    let translatedString = ""
+    for(let i = 0; i < phrase.length; i++) {
+        translatedString += replacement[phrase.charAt(i)] || phrase[i]
+    }
+    return translatedString
+}
+
+function removeKey(message) {
+    const key = /[Space]/g
+    return message.replace(key, ' ')
+}
+
+function reverseMessage(message) {
+    return message.split("").reverse().join("")
+}
+
+const secretMessage = "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs"
+
+console.log(fixChars(removeKey(reverseMessage(secretMessage))))
