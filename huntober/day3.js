1a511525d45d46f8e82b8ed348d4649b062d6d65
ruinaz90 <rzhao9002@gmail.com>
2022-10-07 18:27:40 -0400

    updated function

--- huntober/day3.js
+++ huntober/day3.js
@@ -1,8 +1,7 @@
 // replace any instances of any of the key characters in a given string with an empty space (' ')
 
 function removeKey(message) {
-    const key = /[Space]/g
-    //const key = /[AKEY_!]/g
+    const key = /[AKEY_!]/g
     return message.replace(key, ' ')
 }
 
