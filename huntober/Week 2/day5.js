2792ec500b3e30b56811c61c089e1d3a6e6c4a2d
ruinaz90 <rzhao9002@gmail.com>
2022-10-13 19:32:54 +0000

    Week 2 Day 5

---
+++ huntober/Week 2/day5.js
@@ -0,0 +1,9 @@
+//create a function that will take in an array like we've been working with all week (X-inner arrays of X-primitive values each) and shuffle it up in place. The inner primitive values can move to any of the inner arrays, shuffle away!
+
+function shuffleArray(array) {
+    
+    return array
+}
+
+const someArray = [["Daisy", "Jemma", "Yoyo"], ["Meldina", "Phil", "Leo"]]
+console.log(shuffleArray(someArray))
