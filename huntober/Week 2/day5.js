4e8a8a5fdf4e3e5b1f85bb6d9d2210571c0fe2af
ruinaz90 <rzhao9002@gmail.com>
2022-10-14 18:02:43 +0000

    Week 2 Day 5

--- huntober/Week 2/day5.js
+++ huntober/Week 2/day5.js
@@ -1,7 +1,19 @@
 //create a function that will take in an array like we've been working with all week (X-inner arrays of X-primitive values each) and shuffle it up in place. The inner primitive values can move to any of the inner arrays, shuffle away!
+function shuffleArrayOriginal(array) {
+    for(let i = 0; i < array.length; i++) {
+        let tempIndex = Math.floor(Math.random() * array.length)
+        let temp = array[i]
+        array[i] = array[tempIndex]
+        array[tempIndex] = temp
+    }
+    return array
+}
 
 function shuffleArray(array) {
-    
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
     return array
 }
 
